443d1c8213da944c165e0414ce4687df3e60bd2f
juhee <123529128+JuheeeKim@users.noreply.github.com>
2024-05-21 23:48:20 +0900

    GCar.java 업데이트

--- src/main/java/proj/core/car/GCar.java
+++ src/main/java/proj/core/car/GCar.java
@@ -34,56 +34,29 @@ public class GCar {
         return carId;
     }
 
-    public void setCarId(Long carId) {
-        this.carId = carId;
-    }
-
     public String getPhoneNum() {
         return phoneNum;
-    }
-
-    public void setPhoneNum(String phoneNum) {
-        this.phoneNum = phoneNum;
     }
 
     public int getYear() {
         return year;
     }
 
-    public void setYear(int year) {
-        this.year = year;
-    }
-
     public int getMonth() {
         return month;
     }
 
-    public void setMonth(int month) {
-        this.month = month;
-    }
 
     public int getDate() {
         return date;
-    }
-
-    public void setDate(int date) {
-        this.date = date;
     }
 
     public int getHour() {
         return hour;
     }
 
-    public void setHour(int hour) {
-        this.hour = hour;
-    }
-
     public int getMinute() {
         return minute;
-    }
-
-    public void setMinute(int minute) {
-        this.minute = minute;
     }
 
     public int getParkingNum() {
